6931ec420cc28ef3033f7b93615c76a0ba206312
Diyorbek Sadullaev <diyorbek@mysuperdispatch.com>
2021-01-19 20:28:15 +0100

    add move left, up, down method of player

--- src/Game.java
+++ src/Game.java
@@ -3,14 +3,13 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class Game {
-    private static int i = 0;
     private static Timer tmr = new Timer();
 
     private static Board board = new Board();
     private static Player player = new Player('█', board);
 
     public static void main(String[] args) {
-        tmr.schedule(new MainLoop(), 0, 300);
+        tmr.schedule(new MainLoop(), 0, 100);
     }
 
     static class MainLoop extends TimerTask {
@@ -38,13 +37,31 @@ public class Game {
                 if (System.in.available() != 0) {
                     int c = System.in.read();
 
-                    if (c == 'd') {
-                        player.moveRight(board);
-                    }
+                    switch (c) {
+                        case 'd':
+                            player.moveRight();
+                            break;
 
-                    if (c == 'q') {
-                        tmr.cancel();
-                        tmr.purge();
+                        case 'a':
+                            player.moveLeft();
+                            break;
+
+                        case 'w':
+                            player.moveUp();
+                            break;
+
+                        case 's':
+                            player.moveDown();
+                            break;
+
+                        case 'q': {
+                            tmr.cancel();
+                            tmr.purge();
+                            break;
+                        }
+
+                        default:
+                            break;
                     }
                 }
             } catch (IOException e) {
--- src/Player.java
+++ src/Player.java
@@ -1,21 +1,48 @@
 public class Player {
-    private int[] currentPosition = new int[]{0, 0};
-    private char avatar;
+    private final int[] currentPosition = new int[]{0, 0};
+    private final char avatar;
+    private final Board board;
 
     Player(char avatar, Board board) {
         this.avatar = avatar;
-        board.drawElement(currentPosition[0], currentPosition[1], this.avatar);
+        this.board = board;
+
+        this.board.drawElement(currentPosition[0], currentPosition[1], this.avatar);
     }
 
     public int[] getCurrentPosition() {
         return currentPosition;
     }
 
-    public void moveRight(Board board) {
-        if (board.isCellAvailable(currentPosition[0], currentPosition[1] + 1)) {
-            board.removeElement(currentPosition[0], currentPosition[1]);
+    public void moveRight() {
+        if (this.board.isCellAvailable(currentPosition[0], currentPosition[1] + 1)) {
+            this.board.removeElement(currentPosition[0], currentPosition[1]);
             ++currentPosition[1];
-            board.drawElement(currentPosition[0], currentPosition[1], this.avatar);
+            this.board.drawElement(currentPosition[0], currentPosition[1], this.avatar);
+        }
+    }
+
+    public void moveLeft() {
+        if (this.board.isCellAvailable(currentPosition[0], currentPosition[1] - 1)) {
+            this.board.removeElement(currentPosition[0], currentPosition[1]);
+            --currentPosition[1];
+            this.board.drawElement(currentPosition[0], currentPosition[1], this.avatar);
+        }
+    }
+
+    public void moveUp() {
+        if (this.board.isCellAvailable(currentPosition[0] - 1, currentPosition[1])) {
+            this.board.removeElement(currentPosition[0], currentPosition[1]);
+            --currentPosition[0];
+            this.board.drawElement(currentPosition[0], currentPosition[1], this.avatar);
+        }
+    }
+
+    public void moveDown() {
+        if (this.board.isCellAvailable(currentPosition[0] + 1, currentPosition[1] - 1)) {
+            this.board.removeElement(currentPosition[0], currentPosition[1]);
+            ++currentPosition[0];
+            this.board.drawElement(currentPosition[0], currentPosition[1], this.avatar);
         }
     }
 }
